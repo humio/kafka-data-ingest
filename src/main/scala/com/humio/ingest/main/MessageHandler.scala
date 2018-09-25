91ce4c22d58c6f87c36af9feaa701c1a243ce65f
Christian Hvitved <chr@humio.com>
2018-09-25 21:36:19 +0200

    handle events having both a ts and time field

--- src/main/scala/com/humio/ingest/main/MessageHandler.scala
+++ src/main/scala/com/humio/ingest/main/MessageHandler.scala
@@ -33,8 +33,8 @@ object MessageHandler{
 
           val ts: Long =
             json.getFields("ts", "time") match {
-              case Seq(JsNumber(time)) => (time.doubleValue() * 1000).toLong
-              case Seq(JsString(dateTimeStr)) => ZonedDateTime.parse(dateTimeStr, isoDateTimeFormatter).toInstant.toEpochMilli
+              case JsNumber(time) :: _ => (time.doubleValue() * 1000).toLong
+              case JsString(dateTimeStr) :: _ => ZonedDateTime.parse(dateTimeStr, isoDateTimeFormatter).toInstant.toEpochMilli
               case _ => {
                 logger.warn(s"Got event without recognized timestamp. event=${json}")
                 System.currentTimeMillis()
